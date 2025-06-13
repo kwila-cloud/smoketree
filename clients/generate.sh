53495511d1fba63ee98cc7d8721ba67cfa23df54
Addison Emig <addison@kwila.cloud>
2025-06-13 18:19:20 +0000

    chore: remove clients for now

--- clients/generate.sh
+++ clients/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # TODO: run in GitHub Actions
 # NOTE: The API must be running locally on port 8787
-openapi-generator-cli generate -i http://localhost:8787/openapi.json -g typescript-axios -o clients/typescript
-openapi-generator-cli generate -i http://localhost:8787/openapi.json -g dart -o clients/dart
+openapi-generator-cli generate -i http://localhost:8787/openapi.json -g typescript-axios -o clients/typescript --additional-properties "npmName=smoketree"
+openapi-generator-cli generate -i http://localhost:8787/openapi.json -g dart -o clients/dart --additional-properties "pubName=smoketree,packageName=smoketree"
